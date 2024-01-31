58398582672c529c188c11a5b94c92767c476616
Babatunde-Ben <babatundeben110@gmail.com>
2024-01-31 12:09:43 +0100

    feat: added finished work

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,30 +1,31 @@
-import { ReactComponent as City } from "../assets/svgs/city.svg";
+import { ReactComponent as Tv } from "../assets/svgs/tv.svg";
 import { ReactComponent as Workspace } from "../assets/svgs/workspace.svg";
 import { ReactComponent as Kitchen } from "../assets/svgs/kitchen.svg";
 import { ReactComponent as Wifi } from "../assets/svgs/wifi.svg";
 import { ReactComponent as CarPark } from "../assets/svgs/car-park.svg";
+import { ReactComponent as Camera } from "../assets/svgs/camera.svg";
 export const data = {
-  title: "King Bed - Stylish, Bright & Cozy",
+  title: "Tampa Oasis - Cozy",
   main_image:
-    "https://a0.muscache.com/im/pictures/prohost-api/Hosting-17355559/original/9b28aa75-2404-4e76-bd24-f6d80486d03a.jpeg",
+    "https://a0.muscache.com/im/pictures/miso/Hosting-45464957/original/90b83fcb-8bc9-486e-8052-b509b851b8fb.jpeg",
   airbnb_link:
-    "https://www.airbnb.com/rooms/17355559?source_impression_id=p3_1706610286_V5a72yW7w2BSAwHQ",
+    "https://www.airbnb.com/rooms/45464957?source_impression_id=p3_1706610302_7lmQ0q8v4U6yjSwQ",
   location: "Tampa, Florida, United States",
-  distance: "4 mins to Downtown",
-  price: "$271",
+  distance: "9 mins to Downtown",
+  price: "$118 ",
   period: "Night",
-  rating: "(5.0)",
+  rating: "(4.96)",
   no_of_guests: "6",
-  no_of_beds: "2",
+  no_of_beds: "3",
   no_of_baths: "2",
-  no_of_bedrooms: "2",
+  no_of_bedrooms: "3",
   about:
-    "Discover the perfect blend of style, comfort, and convenience at our 3BR 3Bath Tampa Heights Oasis, a gem nestled in the city's heart, just minutes from Downtown Tampa. This vibrant neighborhood boasts excellent restaurants, shops, attractions, and landmarks, making it an ideal base for your Tampa getaway.",
+    "Welcome to our new, modern 3-bed, 2-bath home in Tampa, perfectly positioned for both tranquility and city exploration. Just a short drive from downtown, our home is ideal!",
   amenities: [
     {
       id: 1,
-      icon: City,
-      text: "City skyline view",
+      icon: Tv,
+      text: "Tv",
     },
     {
       id: 2,
@@ -34,12 +35,12 @@ export const data = {
     {
       id: 3,
       icon: Wifi,
-      text: "Fast wifi – 634 Mbps",
+      text: "Wifi",
     },
     {
       id: 4,
-      icon: Kitchen,
-      text: "Kitchen",
+      icon: Camera,
+      text: "Security cameras on property",
     },
     {
       id: 5,
@@ -53,9 +54,9 @@ export const data = {
     },
   ],
   gallery: [
-    "https://a0.muscache.com/im/pictures/miso/Hosting-17355559/original/da7f2674-bd14-40fe-b1c4-f5906f301ad5.jpeg",
-    "https://a0.muscache.com/im/pictures/miso/Hosting-17355559/original/c90928fb-5c50-4f48-8ea6-7d56b93ef26a.jpeg",
-    "https://a0.muscache.com/im/pictures/miso/Hosting-17355559/original/1fd78880-c084-49af-b4c4-05fa5b3956ae.jpeg",
-    "https://a0.muscache.com/im/pictures/miso/Hosting-17355559/original/2293d6c0-930d-4a6b-b25a-bb57554c86a6.jpeg",
+    "https://a0.muscache.com/im/pictures/miso/Hosting-45464957/original/0286a18a-51bc-4c8b-8aa0-50f64e95499e.jpeg",
+    "https://a0.muscache.com/im/pictures/miso/Hosting-45464957/original/112fdfe9-e56b-45f0-973f-78d15f7f2c79.jpeg",
+    "https://a0.muscache.com/im/pictures/miso/Hosting-45464957/original/bd1b8819-ac03-4f64-9620-3526423b51b1.jpeg",
+    "https://a0.muscache.com/im/pictures/miso/Hosting-45464957/original/114c6bc7-1c74-4c35-948a-544a22ad4c23.jpeg?im_w=720",
   ],
 };
